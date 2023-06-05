cd63538d3bba2b9c31adf2c45f7e92a2242f5229
booobooyyy <hm0ejd74@gmail.com>
2023-06-05 09:07:02 +0800

    Qr Code generator
    Generates a qr code whatever you want by entering a link

---
+++ qr.py
@@ -0,0 +1,19 @@
+import qrcode
+
+def generate_qr_code(data, file_name):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(file_name)
+
+data = input("Enter a link you want to create a code: ")
+file_name = input("Enter a name you want to your code: ")
+
+generate_qr_code(data, file_name)
